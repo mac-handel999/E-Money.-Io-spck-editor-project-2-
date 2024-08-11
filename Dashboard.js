6ac4b3a87e69fe97885310b0c4b4037c4c391d59
mac-handel999 <fabikechukwuemeana@gmail.com>
2024-08-11 21:15:16 +0100

    update version 2

--- Dashboard.js
+++ Dashboard.js
@@ -1,23 +1,26 @@
+const showbtn = document.querySelector('.icon-show');
+const img = document.querySelector('.show-icon');
+const amount = document.querySelector('.amount');
+const amountIn = document.querySelector('.amount-in');
+const amountOut = document.querySelector('.amount-out');
 
-    const showbtn = document.querySelector('.icon-show');
+showbtn.addEventListener('click', () => {
+  if (amount.textContent === "$200,000.00") {
+    amount.textContent = "*****";
     
-    const img = document.querySelector('.show-icon');
+    amountIn.textContent = "*****";
     
-    const amount = document.querySelector('.amount');
+    amountOut.textContent = "*****";
     
-    showbtn.addEventListener('click', ( ) => {
-      if (amount.innerHTML=="&dollar; 200,000.00") {
-        amount.innerHTML = "*****";
-        
-        img.src =' /icons img/Show.png';
-        
-        
-      } else {
-        amount.innerHTML = "&dollar; 200,000.00";
-        
-        img.src =' /icons img/hide.png';
-        
-      }
-      
-      
-    });
+    img.src = 'icons img/Show.png';
+    
+  } else {
+    amount.textContent = "$200,000.00";
+    
+    amountIn.textContent = "Transaction Received From...Of  +$300,000.00 "
+    
+    amountOut.textContent = "Transfer To...Of   -$100,000.00 "
+    
+    img.src = 'icons img/hide.png';
+  }
+});
